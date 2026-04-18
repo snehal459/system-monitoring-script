4d2f87081077ca3dcf29c0eb09370edf830e8ad4
snehal495 <gadgesnehal27@gmail.com>
2026-04-18 07:55:23 +0000

    Fixed disk threshold bug and improved monitoring logic

--- system_monitor.sh
+++ system_monitor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#DISK_THRESHOLD=80
+
+# Thresholds
+DISK_THRESHOLD=80
 MEM_THRESHOLD=75
 LOG_FILE="monitor.log"
 
@@ -7,27 +9,40 @@ echo "------------------------------------"
 echo "System Monitoring Report - $(date)"
 echo "------------------------------------"
 
-DISK_USAGE=$(df / | grep / | awk '{ print $5 }' | sed 's/%//g')
+# -------------------------------
+# Disk Usage
+# -------------------------------
+DISK_USAGE=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 
 echo "Disk Usage: $DISK_USAGE%"
 
-if [ "$DISK_USAGE" -gt "$DISK_THRESHOLD" ]; then
-    echo "ALERT: Disk usage exceeded $DISK_THRESHOLD%" | tee -a $LOG_FILE
+if [ "$DISK_USAGE" -ge "$DISK_THRESHOLD" ]; then
+    MESSAGE="ALERT: Disk usage exceeded $DISK_THRESHOLD%"
+    echo "$MESSAGE" | tee -a $LOG_FILE
 fi
 
-MEM_USAGE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+# -------------------------------
+# Memory Usage
+# -------------------------------
+MEM_USAGE=$(free | awk '/Mem/ {printf("%.0f"), $3/$2 * 100}')
 
-MEM_USAGE_INT=${MEM_USAGE%.*}
+echo "Memory Usage: $MEM_USAGE%"
 
-echo "Memory Usage: $MEM_USAGE_INT%"
-
-if [ "$MEM_USAGE_INT" -gt "$MEM_THRESHOLD" ]; then
-    echo "ALERT: Memory usage exceeded $MEM_THRESHOLD%" | tee -a $LOG_FILE
+if [ "$MEM_USAGE" -ge "$MEM_THRESHOLD" ]; then
+    MESSAGE="ALERT: Memory usage exceeded $MEM_THRESHOLD%"
+    echo "$MESSAGE" | tee -a $LOG_FILE
 fi
+
+# -------------------------------
+# Top CPU Processes
+# -------------------------------
 echo ""
 echo "Top 5 CPU Consuming Processes:"
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -6
 
+# -------------------------------
+# Top Memory Processes (Bonus)
+# -------------------------------
 echo ""
 echo "Top 5 Memory Consuming Processes:"
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -6
